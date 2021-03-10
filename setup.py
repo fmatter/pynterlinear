c55029c8d7f4add0acc2c6ac847c54fb987fc18d
fmatter <florianmatter@gmail.com>
2021-03-10 20:45:39 +0100

    added command hooks

--- setup.py
+++ setup.py
@@ -1,25 +1,2 @@
 import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pynterlinear",
-    version="0.0.1",
-    author="Florian Matter",
-    author_email="florianmatter@gmail.com",
-    description="Converts interlinear text from any source to LaTeX and Microsoft Word",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="-",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "python-docx",
-        "pyperclip"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-)
+setuptools.setup()
